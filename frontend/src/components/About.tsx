9b7b231bf8a6cbbc57fb64b6cc8aaae4864bde00
Anh Vu <vuanhkhoa22002@gmail.com>
2025-10-25 00:46:29 -0700

    about changed

--- frontend/src/components/About.tsx
+++ frontend/src/components/About.tsx
@@ -7,9 +7,9 @@ function About(): React.JSX.Element {
             <Header />
             <div className="about">
                 <h1>About Quiz My Japanese</h1>
-                <p>I made Quiz My Japanese to improve my Japanese reading comprehension. This tool takes a Japanese text passage that you input, and generate a comprehension quiz with the help of AI.</p>
+                <p>I made Quiz My Japanese to improve my Japanese reading skill. You can use this tool for any desired language. </p>
                 <br></br> 
-                <p>To use, simply paste your desired text in the input box and press "Quiz Me!" to test how well you understand the passage. That's it! I hope this tool is useful for you :) </p>
+                <p>To use, simply paste an article or link, and the app generates interactive quizzes to test your comprehension. That's it! I hope this tool is useful for you :) </p>
                 <Link to="/dashboard">Click here use the tool</Link>
             </div>
         </>
