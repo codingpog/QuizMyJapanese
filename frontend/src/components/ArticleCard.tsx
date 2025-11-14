55f0acaea74c47a593d2c2594bc82b2ebb0fa728
Anh Vu <vuanhkhoa22002@gmail.com>
2025-11-14 00:28:22 -0800

    value is declared but never used - fix

--- frontend/src/components/ArticleCard.tsx
+++ frontend/src/components/ArticleCard.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
 //import type { QuizQuestion } from "../types";
 
 function ArticleCard({ article }: any): React.JSX.Element {
